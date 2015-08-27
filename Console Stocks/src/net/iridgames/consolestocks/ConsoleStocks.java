5e1089891633150c13e26fc674212ba353ce6dbf
MightyElemental <minebox30@gmail.com>
2015-08-27 22:08:19 +0100

    Implemented Slick

--- Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
+++ Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
@@ -1,10 +1,17 @@
 package net.iridgames.consolestocks;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 import net.iridgames.consolestocks.client.Client;
+import net.iridgames.consolestocks.gui.StateGame;
+import net.iridgames.consolestocks.gui.StateMenu;
 import net.iridgames.consolestocks.server.Server;
 
 /** @author MightyElemental & WolfgangTS */
-public class ConsoleStocks {
+public class ConsoleStocks extends StateBasedGame {
 
 	public static Server server;
 
@@ -13,9 +20,19 @@ public class ConsoleStocks {
 	public static Client	client;
 	public static boolean	isServer	= false;
 
-	public ConsoleStocks() {
-		if (showGUI) {
+	public static final int	STATE_MENU	= 0;
+	public static final int	STATE_GAME	= 1;
 
+	public static final String	GAME_NAME	= "That good game";
+	public static final String	VERSION		= "0.0.0";
+	public static final String	TITLE		= GAME_NAME + " | Version " + VERSION;
+	public static final int		WIDTH		= 1600;
+
+	public ConsoleStocks( String name ) {
+		super(name);
+		if (!isServer) {
+			this.addState(new StateMenu(STATE_MENU));
+			this.addState(new StateGame(STATE_GAME));
 		}
 	}
 
@@ -25,6 +42,7 @@ public class ConsoleStocks {
 				if (settings[i] != null) {
 					switch (settings[i]) {
 						case "--nogui":
+							showGUI = false;
 							break;
 						case "--port":
 							i++;
@@ -39,12 +57,31 @@ public class ConsoleStocks {
 				}
 			}
 
-			if (!isServer) {
-				// do stuff
+			if (!isServer && showGUI) {
+				setupClient();
 			}
 		} catch (Exception e) {
 		}
-		new ConsoleStocks();
+
+	}
+
+	private static void setupClient() {
+		AppGameContainer appGc;
+		try {
+			appGc = new AppGameContainer(new ConsoleStocks(TITLE));
+			appGc.setDisplayMode(WIDTH, (int) (WIDTH / 16.0 * 9.0), false);
+			appGc.setTargetFrameRate(60);
+			appGc.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		this.getState(STATE_MENU).init(gc, this);
+		this.getState(STATE_GAME).init(gc, this);
+		this.enterState(STATE_MENU);
 	}
 
 }
