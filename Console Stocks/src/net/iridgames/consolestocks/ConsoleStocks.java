10d69b79b17f562146369b296dd44d88a98d6073
MightyElemental <minebox30@gmail.com>
2015-08-27 19:31:30 +0100

    fghj

--- Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
+++ Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
@@ -1,10 +1,11 @@
 package net.iridgames.consolestocks;
 
-
+/** @author MightyElemental & WolfgangTS
+ * @since 26/08/15 */
 public class ConsoleStocks {
 
 	public static void main(String[] args) {
-		 
+		
 	}
 
 }
