d249111026a66ed325c59e7094d89f6f90d206a7
WolfgangTS <lobo.noe@gmail.com>
2015-08-27 21:00:19 +0200

    dsadsd

--- Console Stocks/src/net/iridgames/consolestocks/client/Client.java
+++ Console Stocks/src/net/iridgames/consolestocks/client/Client.java
@@ -2,5 +2,34 @@ package net.iridgames.consolestocks.client;
 
 
 public class Client {
-
+	private String userName;
+	private String address;
+	private int port;
+	
+	public Client(String name, String address, int port)
+	{
+		this.userName = name;
+		this.address = address;
+		this.port = port;
+	}
+	
+	public String getName()
+	{
+		return this.userName;
+	}
+	
+	public String getAddress()
+	{
+		return this.getAddress();
+	}
+	
+	public int getPort()
+	{
+		return this.getPort();
+	}
+	
+	public void connect()
+	{
+		
+	}
 }
