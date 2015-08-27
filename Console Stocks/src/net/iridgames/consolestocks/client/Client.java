619acf75fa62cde1c46a7b1be4fdb9997e33588a
WolfgangTS <lobo.noe@gmail.com>
2015-08-27 22:23:35 +0200

    sd

--- Console Stocks/src/net/iridgames/consolestocks/client/Client.java
+++ Console Stocks/src/net/iridgames/consolestocks/client/Client.java
@@ -1,35 +1,83 @@
 package net.iridgames.consolestocks.client;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-public class Client {
-	private String userName;
-	private String address;
-	private int port;
-	
+public class Client
+{
+	private String	userName;
+	private String	address;
+	private int		port;
+
+	private DatagramSocket clientSocket;
+
+	private InetAddress IPAddress;
+
+	private byte[]	receiveData;
+	private byte[]	sendData;
+
 	public Client(String name, String address, int port)
 	{
 		this.userName = name;
 		this.address = address;
 		this.port = port;
 	}
-	
+
 	public String getName()
 	{
 		return this.userName;
 	}
-	
+
 	public String getAddress()
 	{
-		return this.getAddress();
+		return this.address;
 	}
-	
+
 	public int getPort()
 	{
 		return this.getPort();
 	}
-	
-	public void connect()
+
+	public void sendMessage(String message)
 	{
+		String messageOut = this.userName + " : " + message;
 		
+		try
+		{
+			clientSocket = new DatagramSocket();
+			IPAddress = InetAddress.getByName(getAddress());
+
+			sendData = new byte[1024];
+			receiveData = new byte[1024];
+			
+			sendData = messageOut.getBytes();
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, this.port);
+			try
+			{
+				clientSocket.send(sendPacket);
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+
+				String receiveData = new String(receivePacket.getData()).trim();
+				
+				System.out.println(receiveData);
+
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		catch (SocketException | UnknownHostException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+
 	}
 }
--- Console Stocks/src/net/iridgames/consolestocks/server/Server.java
+++ Console Stocks/src/net/iridgames/consolestocks/server/Server.java
@@ -1,30 +1,78 @@
 package net.iridgames.consolestocks.server;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Server
 {
-	private String	address;
 	private int		port;
+	private boolean	running;
+
+	private DatagramSocket serverSocket;
+
+	private byte[]	receiveData;
+	private byte[]	sendData;
 
 	private Thread serverTick = new Thread()
 	{
 		public void run()
 		{
-			System.out.println(port);
+						
+			receiveData = new byte[1024];
+			sendData = new byte[1024];
+
+			while (running)
+			{
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				try
+				{
+					serverSocket.receive(receivePacket);
+					String data = new String(receivePacket.getData()).trim();
+					
+					InetAddress IPAddress = receivePacket.getAddress();
+
+					int port = receivePacket.getPort();
+
+					String[] dataArray = data.split(" : ");
+					
+					StringBuilder sb = new StringBuilder();
+					
+					for (int i = 1; i < dataArray.length; i++)
+					{
+						sb.append(dataArray[i]);
+					}
+					
+					sendData = sb.toString().toUpperCase().getBytes("UTF-8");
+					
+					DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+					
+					
+					
+					serverSocket.send(sendPacket);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			try
+			{
+				this.join();
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	};
 
-	public Server(String address, int port)
+	public Server(int port)
 	{
-		this.address = address;
 		this.port = port;
-	}
-
-	public String getAddress()
-	{
-		return this.address;
+		this.running = true;
 	}
 
 	public int getPort()
@@ -36,15 +84,13 @@ public class Server
 	{
 		try
 		{
-			DatagramSocket ServerSocket = new DatagramSocket(this.port);
-
-			byte[] receiveData = new byte[1024];
-			byte[] sendData = new byte[1024];
-
-			serverTick.start();
+			serverSocket = new DatagramSocket(port);
 		}
-		catch (SocketException e)
+		catch (SocketException e1)
 		{
+			e1.printStackTrace();
 		}
+
+		serverTick.start();
 	}
 }
