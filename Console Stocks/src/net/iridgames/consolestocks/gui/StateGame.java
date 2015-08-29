46073efb36bb8cebf7d97c7ca2d820a3d26e99fc
MightyElemental <minebox30@gmail.com>
2015-08-29 14:21:29 +0100

    asdfg

--- Console Stocks/src/net/iridgames/consolestocks/gui/StateGame.java
+++ Console Stocks/src/net/iridgames/consolestocks/gui/StateGame.java
@@ -28,6 +28,7 @@ public class StateGame extends BasicGameState {
 	public String	commandLine		= "";
 	public String	dispCommandLine	= prefix;
 	public int		cursor			= 0;
+	public int		pastComCur		= 0;
 
 	public StateGame( int id ) {
 		this.ID = id;
@@ -65,11 +66,13 @@ public class StateGame extends BasicGameState {
 	public void renderTR(GameContainer gc, StateBasedGame sbg, Graphics g) {
 		final int xDisp = gc.getWidth() / 2;
 		final int yDisp = 0;
+		renderConsole(gc, sbg, g, xDisp, yDisp);
 	}
 
 	public void renderBR(GameContainer gc, StateBasedGame sbg, Graphics g) {
 		final int xDisp = gc.getWidth() / 2;
 		final int yDisp = gc.getHeight() / 2;
+		renderConsole(gc, sbg, g, xDisp, yDisp);
 	}
 
 	public void renderBL(GameContainer gc, StateBasedGame sbg, Graphics g) {
@@ -91,8 +94,7 @@ public class StateGame extends BasicGameState {
 				tempY = (20 * (iStop - iStart));
 			}
 		}
-		System.out.println(iStart);
-		g.drawString(dispCommandLine + "\n" + commandLine + ".\n" + cursor + "\n" + commandLine.length(), x + 10, y + tempY);
+		g.drawString(dispCommandLine, x + 10, y + tempY);
 	}
 
 	private float ticks;
@@ -159,6 +161,29 @@ public class StateGame extends BasicGameState {
 			cursor++;
 			updateCursor(sb.length());
 		}
+		if (keyCodePressed == Input.KEY_UP) {
+			if (commands.size() > 0) {
+				pastComCur++;
+				if (commands.size() - 1 - pastComCur < 0) {
+					pastComCur = commands.size() - 1;
+				}
+				sb.delete(0, sb.length());
+				sb.append(commands.get(commands.size() - 1 - pastComCur));
+			}
+			cursor = sb.length();
+		}
+		if (keyCodePressed == Input.KEY_DOWN) {
+			if (commands.size() > 0) {
+				pastComCur--;
+				if (commands.size() - 1 - pastComCur > commands.size() - 1) {
+					pastComCur = 0;
+				}
+				sb.delete(0, sb.length());
+				sb.append(commands.get(commands.size() - 1 - pastComCur));
+			}
+			cursor = sb.length();
+		}
+
 		if (keyCodePressed == Input.KEY_ENTER) {
 			ConsoleStocks.client.sendMessage(sb.toString());
 			commands.add(sb.toString());
@@ -166,9 +191,12 @@ public class StateGame extends BasicGameState {
 			sb.delete(0, sb.length());
 			updateCursor(sb.length());
 		}
+		System.out.println(sb.length());
 		commandLine = sb.toString();
 		// GO AT END
+		System.out.println(sb.toString());
 		dispCommandLine = prefix + sb.toString();
+
 	}
 
 	public void updateCursor(int length) {
