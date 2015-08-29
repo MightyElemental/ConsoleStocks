95c61023a406d0c985146aa0480539e219723247
MightyElemental <minebox30@gmail.com>
2015-08-29 17:40:50 +0100

    hi

--- Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
+++ Console Stocks/src/net/iridgames/consolestocks/ConsoleStocks.java
@@ -25,7 +25,7 @@ public class ConsoleStocks extends StateBasedGame {
 	public static final int	STATE_GAME	= 1;
 
 	public static final String	GAME_NAME	= "Console Stocks";
-	public static final String	VERSION		= "0.0.0";
+	public static final String	VERSION		= "0.2.1";
 	public static final String	TITLE		= GAME_NAME + " | v" + VERSION;
 	public static final int		WIDTH		= 1600;
 
--- Console Stocks/src/net/iridgames/consolestocks/server/Parser.java
+++ Console Stocks/src/net/iridgames/consolestocks/server/Parser.java
@@ -3,6 +3,7 @@ package net.iridgames.consolestocks.server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class Parser {
@@ -43,6 +44,9 @@ public class Parser {
 				case "PING":
 					sendMessage("PONG!", ip, port);
 					break;
+				case "MSG":
+					messageUser(msg, ip, port);
+					break;
 				default:
 					sendMessage("Invalid command.", ip, port);
 					break;
@@ -50,6 +54,22 @@ public class Parser {
 		} catch (Exception e) {
 			e.printStackTrace();
 			sendMessage("Internal Server Error.", ip, port);
+		}
+	}
+
+	public void messageUser(String[] commands, InetAddress ip, int port) throws UnknownHostException {
+		if (commands.length < 3) { return; }
+		InetAddress sendIP = InetAddress.getByName(commands[1]);
+		String sendMessage = ip.getHostAddress() + "> ";
+		for (int i = 2; i < commands.length; i++) {
+			sendMessage += commands[i]+" ";
+		}
+		try {
+			server.sendData = (sendMessage.toString()).getBytes("UTF-8");
+			DatagramPacket sendPacket = new DatagramPacket(server.sendData, server.sendData.length, sendIP, port);
+			server.serverSocket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
