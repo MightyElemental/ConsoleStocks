96f8b02a97b73d07bb461e0a133bf768a151a262
MightyElemental <minebox30@gmail.com>
2015-09-02 16:53:48 +0100

    g

--- Console Stocks/src/net/iridgames/consolestocks/server/ServerGUI.java
+++ Console Stocks/src/net/iridgames/consolestocks/server/ServerGUI.java
@@ -34,12 +34,6 @@ public class ServerGUI extends JFrame {
 
 	private JPanel contentPane;
 
-	public int	maxMemory;
-	public int	totalMemory;
-	public int	freeMemory;
-
-	JProgressBar currentRam;
-
 	/** Create the frame. */
 	public ServerGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -53,34 +47,11 @@ public class ServerGUI extends JFrame {
 		panel.setBounds(0, 0, 608, 374);
 		contentPane.add(panel);
 		panel.setLayout(null);
-
-		currentRam = new JProgressBar();
-		currentRam.setBounds(10, 11, 193, 37);
-		panel.add(currentRam);
-
-		JLabel lblCurrentRamUsage = new JLabel("Current RAM Usage");
-		lblCurrentRamUsage.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentRamUsage.setBounds(10, 48, 193, 14);
-		panel.add(lblCurrentRamUsage);
 		setVisible(true);
 		serverGUIThread.start();
 	}
 
 	public void update() {
-		Runtime rt = Runtime.getRuntime();
-		long maxMem = rt.maxMemory();
-		long totalMem = rt.totalMemory();
-		long freeMem = rt.freeMemory();
 
-		maxMemory = (int) (maxMem / 1024);
-		totalMemory = (int) (totalMem / 1024);
-		freeMemory = (int) (freeMem / 1024);
-
-		System.out.println(maxMemory + "KB | " + totalMemory + "KB | " + freeMemory + "KB");
-		updateRAMBar();
-	}
-
-	public void updateRAMBar() {
-		currentRam.setValue((int) (((freeMemory + 0.0) / (totalMemory + 0.0)) * 100.0));
 	}
 }
