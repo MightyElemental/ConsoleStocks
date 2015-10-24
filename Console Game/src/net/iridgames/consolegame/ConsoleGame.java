99bb28fcf57d646485c2cfd9f68bdaed985e75b8
WolfgangTS <lobo.noe@gmail.com>
2015-10-24 11:11:52 +0200

    sd

--- Console Game/src/net/iridgames/consolegame/ConsoleGame.java
+++ Console Game/src/net/iridgames/consolegame/ConsoleGame.java
@@ -4,39 +4,47 @@ import java.net.InetAddress;
 import java.util.Random;
 
 import net.iridgames.consolegame.io.Input;
+import net.iridgames.consolegame.io.Output;
 import net.mightyelemental.network.Server;
 import net.mightyelemental.network.client.Client;
 import net.mightyelemental.network.listener.MessageListenerClient;
 import net.mightyelemental.network.listener.MessageListenerServer;
 
-public class ConsoleGame implements MessageListenerServer, MessageListenerClient {
+public class ConsoleGame implements MessageListenerServer, MessageListenerClient
+{
 
-	public static Random random = new Random();
+	public static Random	random	= new Random();
 
 	public static Server	server;
 	public static Client	client	= new Client("localhost", 4040);
 	String[]				args2;
+	
+	public static Input		input	= new Input();
+	public static Output	output	= new Output();
 
-	public ConsoleGame( String[] args ) {
-		
-		
-		
+	public ConsoleGame(String[] args)
+	{
+
 		// if (SteamAPI.init()) {
 		// System.out.println("did not init");
 		// }
 		args2 = args;
-		if (args.length > 0 && args[0].equals("--server")) {
+		if (args.length > 0 && args[0].equals("--server"))
+		{
 			System.out.println("Server");
 			server = new Server(4040);
 			server.setupServer();
 			server.addListener(this);
 			server.initGUI("Server Stuffs");
-		} else {
+		}
+		else
+		{
 			System.out.println("Client");
 			client.setup();
 			client.addListener(this);
 			String input = Input.getInputText();
-			while (!input.equals("exit()")) {
+			while (!input.equals("exit()"))
+			{
 				client.sendMessage(input);
 				input = Input.getInputText();
 			}
@@ -46,12 +54,14 @@ public class ConsoleGame implements MessageListenerServer, MessageListenerClient
 		// System.out.println(Input.getInputText());
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		new ConsoleGame(args);
 	}
 
 	@Override
-	public void onMessageRecievedFromClient(String message, InetAddress ip, int port) {
+	public void onMessageRecievedFromClient(String message, InetAddress ip, int port)
+	{
 		// System.out.println("(" + args2[0] + ") new message! " + message);
 		// for (String arg : Parser.getArgs(message)) {
 		// System.out.println(arg);
@@ -61,15 +71,19 @@ public class ConsoleGame implements MessageListenerServer, MessageListenerClient
 	}
 
 	@Override
-	public void onNewClientAdded(InetAddress ip, int port, String uid) {
+	public void onNewClientAdded(InetAddress ip, int port, String uid)
+	{
 		System.out.println(ip.getHostAddress() + ":" + port + " has connected");
-		// server.broadcastmessage("(bc) hello new client! your new UID is " + uid);
-		// server.sendMessage("(direct) hello new client! your new UID is " + uid, ip, port);
+		// server.broadcastmessage("(bc) hello new client! your new UID is " +
+		// uid);
+		// server.sendMessage("(direct) hello new client! your new UID is " +
+		// uid, ip, port);
 	}
 
 	@Override
-	public void onMessageRecievedFromServer(String message) {
-		System.out.println(message);
+	public void onMessageRecievedFromServer(String message)
+	{
+
 	}
 
 }
